e7c7b5ea65f9ad71c74c79732e1843db5fad406a
Aoi <41570034+MororoAoi@users.noreply.github.com>
2024-03-18 11:23:49 +0900

    Update config.js

--- themes/hexo/config.js
+++ themes/hexo/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  HEXO_HOME_BANNER_ENABLE: false,
+  HEXO_HOME_BANNER_ENABLE: true,
   // 3.14.1以后的版本中，欢迎语在blog.config.js中配置，用英文逗号','隔开多个。
   HEXO_HOME_BANNER_GREETINGS: ['健康，活力，美好, 每一个词都属于你 🩷, 欢迎来到我的博客🎉'], // 首页大图标语文字
 
