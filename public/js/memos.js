7cfdb6666e9b946aef0f6984d884db6341b65ec5
Aoi <41570034+MororoAoi@users.noreply.github.com>
2024-03-25 12:45:05 +0900

    Update memos.js

--- public/js/memos.js
+++ public/js/memos.js
@@ -51,8 +51,8 @@ var memo = {
     limit: '15',
     creatorId: '1',
     domId: '#memos',
-    username: 'Aoi',
-    name: 'Aoi的生活碎片'
+    username: 'Aoi的生活碎片',
+    name: 'Aoi'
 }
 if (typeof (memos) !== "undefined") {
     for (var key in memos) {
