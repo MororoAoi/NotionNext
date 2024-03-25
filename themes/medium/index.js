0ac8ca32bf2081071756e60ac39ff19cba648e71
Aoi <41570034+MororoAoi@users.noreply.github.com>
2024-03-25 11:01:23 +0900

    Update index.js

--- themes/medium/index.js
+++ themes/medium/index.js
@@ -282,24 +282,25 @@ const LayoutMemos = (props) => {
   const memoPageInfo = {
     id: "125fe5e1704d4d7a9615661925d3e66a", // 因为引入了评论互动，所以需要一个ID来对应加载页面评论，这里使用Notion这个菜单的pageID
     type: "Memos",
-    title: "说说",
+    title: "说说"
   };
-  return  (   
-	{/* 这里不是hexo主题的话，return部分不要照搬，最好参考index.js 文章详情 LayoutSlug的模块移植 */}
-  <div class="w-full lg:hover:shadow rounded-md lg:rounded-md lg:px-2 lg:py-4 article">
-		{/* 去掉加密的Lock部分判断 */}
-    <div id="article-wrapper" class="overflow-x-auto flex-grow mx-auto md:w-full px-3 font-serif">  
-      <article itemScope itemType="https://schema.org/Movie" class="subpixel-antialiased overflow-y-hidden overflow-x-hidden" >
-        {/* Notion文章主体 */}
+	
+  return  (   	
+  <div className="w-full lg:hover:shadow rounded-md lg:rounded-md lg:px-2 lg:py-4 article">
+		
+    <div id="article-wrapper" className="overflow-x-auto flex-grow mx-auto md:w-full px-3 font-serif">  
+      <article itemScope itemType="https://schema.org/Movie" className="subpixel-antialiased overflow-y-hidden overflow-x-hidden" >
+        
         <section class='justify-center mx-auto max-w-2xl lg:max-w-full'>
             <BlogMemos {...props}/>
         </section>
       </article>
-			{/* 移除了分享模块，如果需要可以保留，将 LayoutSlug的对应部分拷贝过来 */}
+			
       <div class='pt-4 border-dashed'></div>
       
     </div>
-  </div>);
+  </div>
+ );
 };
 
 /**
