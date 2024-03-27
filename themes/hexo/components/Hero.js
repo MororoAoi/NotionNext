ccbbad5d480ac7383b00b8ace357da5627bc7e2d
Aoi <41570034+MororoAoi@users.noreply.github.com>
2024-03-27 16:21:45 +0900

    Update Hero.js

--- themes/hexo/components/Hero.js
+++ themes/hexo/components/Hero.js
@@ -74,20 +74,6 @@ const Hero = props => {
                 </div>
             </div>
 
-{/* 波浪效果 */}
-          <div id="waves" className="absolute bottom-0 w-full z-10">
-            <svg className="waves" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shapeRendering="auto">
-              <defs>
-                <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
-              </defs>
-              <g className="parallax">
-                <use xlinkHref="#gentle-wave" x="48" y="0" />
-                <use xlinkHref="#gentle-wave" x="48" y="3" />
-                <use xlinkHref="#gentle-wave" x="48" y="5" />
-                <use xlinkHref="#gentle-wave" x="48" y="7" />
-              </g>
-            </svg>
-          </div>
               
             <LazyImage id='header-cover' src={siteInfo?.pageCover}
                 className={`header-cover w-full h-screen object-cover object-center ${siteConfig('HEXO_HOME_NAV_BACKGROUND_IMG_FIXED', null, CONFIG) ? 'fixed' : ''}`} />
