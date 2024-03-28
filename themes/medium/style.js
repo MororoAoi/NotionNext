e8cf7c0f4fa13339cc4a146720f76511e38475fe
Aoi <41570034+MororoAoi@users.noreply.github.com>
2024-03-28 12:40:54 +0900

    Update style.js

--- themes/medium/style.js
+++ themes/medium/style.js
@@ -5,14 +5,20 @@
  * @returns
  */
 const Style = () => {
-  return <style jsx global>{`
-    
+  return (
+    <style jsx global>{`
+
+    body{
+     background-image: url('https://images.pexels.com/photos/3394939/pexels-photo-3394939.jpeg');
+        background-size: cover; // 确保图片覆盖整个背景
+    }
     // 底色
     .dark body{
         background-color: black;
     }
 
   `}</style>
+);
 }
 
 export { Style }
