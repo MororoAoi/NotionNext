cb6aa90c61dd79475df2b65de6daba7bd99dcd27
Aoi <41570034+MororoAoi@users.noreply.github.com>
2024-03-28 14:25:33 +0900

    Update Footer.js

--- themes/medium/components/Footer.js
+++ themes/medium/components/Footer.js
@@ -11,7 +11,7 @@ const Footer = ({ title }) => {
     <footer
       className='z-10 dark:bg-hexo-black-gray flex-shrink-0 justify-center text-center m-auto w-full leading-6 text-sm p-6 relative'
     >
-       <DarkModeButton/>
+       
       <i className='fas fa-copyright' /> {`${copyrightDate}`} <span><i className='mx-1 animate-pulse fas fa-heart'/> <a href={siteConfig('LINK')} className='underline font-bold text-gray-500 dark:text-gray-300 '>{siteConfig('AUTHOR')}</a>.<br/>
 
       {siteConfig('BEI_AN') && <><i className='fas fa-shield-alt'/> <a href='https://beian.miit.gov.cn/' className='mr-2'>{siteConfig('BEI_AN')}</a><br/></>}
