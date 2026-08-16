16d381910a9e4ba6bfe93544b53cec1bcb75d120
Aoi <41570034+MororoAoi@users.noreply.github.com>
2026-08-16 20:53:46 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,33 +7,26 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: BLOG.BUNDLE_ANALYZER
 })
 
-/**
- * 扫描指定目录下的文件夹名，用于获取所有主题
- * @param {*} directory
- * @returns
- */
 function scanSubdirectories(directory) {
   const subdirectories = []
 
-  fs.readdirSync(directory).forEach(file => {
-    const fullPath = path.join(directory, file)
-    const stats = fs.statSync(fullPath)
-    if (stats.isDirectory()) {
-      subdirectories.push(file)
-    }
+    fs.readdirSync(directory).forEach(file => {
+      const fullPath = path.join(directory, file)
+      const stats = fs.statSync(fullPath)
+      if (stats.isDirectory()) {
+        subdirectories.push(file)
+      }
+    })
 
-    // subdirectories.push(file)
-  })
-
-  return subdirectories
+return subdirectories
 }
-// 扫描项目 /themes下的目录名
 const themes = scanSubdirectories(path.resolve(__dirname, 'themes'))
 module.exports = withBundleAnalyzer({
+  eslint: {
+    ignoreDuringBuilds: true
+  },
   images: {
-    // 图片压缩
     formats: ['image/avif', 'image/webp'],
-    // 允许next/image加载的图片 域名
     domains: [
       'gravatar.com',
       'www.notion.so',
@@ -43,9 +36,8 @@ module.exports = withBundleAnalyzer({
       'p1.qhimg.com',
       'webmention.io',
       'ko-fi.com'
-    ]
+      ]
   },
-  // 默认将feed重定向至 /public/rss/feed.xml
   async redirects() {
     return [
       {
@@ -53,7 +45,7 @@ module.exports = withBundleAnalyzer({
         destination: '/rss/feed.xml',
         permanent: true
       }
-    ]
+      ]
   },
   async rewrites() {
     return [
@@ -61,7 +53,7 @@ module.exports = withBundleAnalyzer({
         source: '/:path*.html',
         destination: '/:path*'
       }
-    ]
+      ]
   },
   async headers() {
     return [
@@ -79,20 +71,11 @@ module.exports = withBundleAnalyzer({
             value:
               'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
           }
-        ]
+          ]
       }
-    ]
+      ]
   },
   webpack: (config, { dev, isServer }) => {
-    // Replace React with Preact only in client production build
-    // if (!dev && !isServer) {
-    //   Object.assign(config.resolve.alias, {
-    //     react: 'preact/compat',
-    //     'react-dom/test-utils': 'preact/test-utils',
-    //     'react-dom': 'preact/compat'
-    //   })
-    // }
-    // 动态主题：添加 resolve.alias 配置，将动态路径映射到实际路径
     if (!isServer) {
       console.log('[加载主题]', path.resolve(__dirname, 'themes', THEME))
     }
@@ -103,12 +86,11 @@ module.exports = withBundleAnalyzer({
     scrollRestoration: true
   },
   exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
-    // 导出时 忽略/pages/sitemap.xml.js ， 否则报错getServerSideProps
     const pages = { ...defaultPathMap }
-    delete pages['/sitemap.xml']
+      delete pages['/sitemap.xml']
     return pages
   },
-  publicRuntimeConfig: { // 这里的配置既可以服务端获取到，也可以在浏览器端获取到
+  publicRuntimeConfig: {
     NODE_ENV_API: process.env.NODE_ENV_API || 'prod',
     THEMES: themes
   }
